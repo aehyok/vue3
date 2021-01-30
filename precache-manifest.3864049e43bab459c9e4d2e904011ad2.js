55582709aa1e283c15e817dff7c6130eb0415ccc
刘启明 <455043818@qq.com>
2021-01-30 17:29:46 +0800

    commit test website

---
+++ precache-manifest.3864049e43bab459c9e4d2e904011ad2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1616e5b9cb2795a9136d95dfec3af864",
+    "url": "/bootvideo.ts"
+  },
+  {
+    "revision": "afc2ab1dda417a236d47754fab121785",
+    "url": "/china1.json"
+  },
+  {
+    "revision": "6555815bd7d361f98f87",
+    "url": "/css/app.5c795ab3.css"
+  },
+  {
+    "revision": "c1d54dd22e324780805d",
+    "url": "/css/chunk-vendors.889b9913.css"
+  },
+  {
+    "revision": "49d9343ecb501b272b04",
+    "url": "/css/echart-map.0534ef79.css"
+  },
+  {
+    "revision": "a7472e216ca549950e15d51c14c4a265",
+    "url": "/flame.avi"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "43445dd7fef61c01881d0b446baaf696",
+    "url": "/img/login_bg.43445dd7.jpg"
+  },
+  {
+    "revision": "c9e2be1350db456bb3bb10ecfb74eb8e",
+    "url": "/img/logo.c9e2be13.png"
+  },
+  {
+    "revision": "361a1a7e769483251c3f03f43d9477c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1923f66435b6e34a6534",
+    "url": "/js/about.466ef3a1.js"
+  },
+  {
+    "revision": "6555815bd7d361f98f87",
+    "url": "/js/app.f42ee333.js"
+  },
+  {
+    "revision": "c1d54dd22e324780805d",
+    "url": "/js/chunk-vendors.90f366c7.js"
+  },
+  {
+    "revision": "49d9343ecb501b272b04",
+    "url": "/js/echart-map.69963147.js"
+  },
+  {
+    "revision": "92c481f3ee3a3507e71a9616a7ff91ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "896345d32839937a74e2f419d763c884",
+    "url": "/test.mp4"
+  }
+]);
